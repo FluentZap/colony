cd03f43aac9c7b62319b1a75c881022765fddafa
Todd Aden <45721115+FluentZap@users.noreply.github.com>
2018-12-03 10:36:07 -0800

    Added Design Documents Added Classes for City, UI and Encyclopedia

---
+++ Colnaught/City.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colnaught
+{
+
+    enum Listof_Structures
+    {
+        Grass,
+        CityCenter,
+        ZoneResidential,
+        ZoneCommercial,
+        ZoneIndustrial,
+        RoadDirt
+
+    }
+
+    
+    class City_Tyle
+    {
+        public Listof_Structures Type = 0;
+
+        public City_Tyle()
+        {
+
+        }
+
+    }
+
+
+    class City
+    {
+        public City_Tyle[,] TileMap = new City_Tyle[200, 200];
+
+        public City()
+        {
+            for (int x = 0; x < 200; x++)
+                for (int y = 0; y < 200; y++)                
+                    TileMap[x, y] = new City_Tyle();
+            
+        }
+
+
+    }
+}
---
+++ Colnaught/Encyclopedia.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colnaught
+{
+
+    enum Listof_BuildTypes
+    {
+        Zone,
+        Road,
+        Structure
+    }
+
+    class Typeof_BuildItems
+    {
+        public int Cost;
+        public Listof_BuildTypes BuildingType;
+        public Listof_Texture Texture;
+    }
+
+
+    class Encyclopedia
+    {
+
+
+        public Dictionary<Listof_Structures, Typeof_BuildItems> Dictionaryof_BuildItems = new Dictionary<Listof_Structures, Typeof_BuildItems>();
+
+
+        public Encyclopedia()
+        {
+            Dictionaryof_BuildItems.Add(Listof_Structures.Grass, new Typeof_BuildItems() { BuildingType = Listof_BuildTypes.Zone, Texture = Listof_Texture.Grass });
+            Dictionaryof_BuildItems.Add(Listof_Structures.CityCenter, new Typeof_BuildItems() { BuildingType = Listof_BuildTypes.Structure, Texture = Listof_Texture.CityCenter });
+            Dictionaryof_BuildItems.Add(Listof_Structures.ZoneResidential, new Typeof_BuildItems() { BuildingType = Listof_BuildTypes.Zone, Texture = Listof_Texture.Residential });
+            Dictionaryof_BuildItems.Add(Listof_Structures.ZoneCommercial, new Typeof_BuildItems() { BuildingType = Listof_BuildTypes.Zone, Texture = Listof_Texture.Commercial });
+            Dictionaryof_BuildItems.Add(Listof_Structures.ZoneIndustrial, new Typeof_BuildItems() { BuildingType = Listof_BuildTypes.Zone, Texture = Listof_Texture.Industrial });
+
+            Dictionaryof_BuildItems.Add(Listof_Structures.RoadDirt, new Typeof_BuildItems() { BuildingType = Listof_BuildTypes.Zone, Texture = Listof_Texture.Road });
+
+        }
+
+
+
+    }
+
+    
+}
